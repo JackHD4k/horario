bc1367ada7ea466e2ef7f3ca8ee56c38dd7bbc12
joelramos <joelramos456@gmail.com>
2022-05-29 18:24:10 -0500

    termino de v1

---
+++ script.js
@@ -0,0 +1,15 @@
+function linux() {
+    Swal.fire('https://teams.microsoft.com/l/meetup-join/19%3ameeting_NDg0MmQ4MWYtNDBhOS00NGEyLWFlY2MtOGY2MDg3MjcyN2Ey%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%226749b2ac-2877-4691-8076-16733497418e%22%7d')
+}
+function fpr(){
+    Swal.fire('https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZmRhZDRkOTUtNTY3Yy00NzQ2LThjMTItZWZjOTE1Nzk4N2Fh%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%226749b2ac-2877-4691-8076-16733497418e%22%7d')
+}
+function android() {
+    Swal.fire('https://teams.microsoft.com/l/meetup-join/19%3ameeting_N2ExZjdhZGYtYzYyMy00Zjc2LTk1ODctMzBiOTY1NjMxYTc1%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%225d229e2e-f411-46f3-919d-98dde7782927%22%7d')
+}
+function admin() {
+    Swal.fire('https://teams.microsoft.com/l/meetup-join/19%3ameeting_MTdhYTRkNDEtYmVmMC00Yzg3LTkyMGQtY2ViYzllZGY3MjRk%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%224056cc8a-ba97-410e-8eb4-dde10b62de99%22%7d')
+}
+function java() {
+    Swal.fire('https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTI3ZDMyNjktZTFiNi00ODMxLThhOTgtMDdmZWEyZGE2NTVl%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%2215ee1801-f34e-493b-bea2-cba2916393d0%22%7d')
+}
